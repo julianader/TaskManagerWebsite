12eeba69b472f208cc2a4cfb9978de506aa9cf28
nancyhussein200 <nancyhussein200@gmail.com>
2023-12-01 17:12:52 +0200

    Function for adding a task with date and time

---
+++ Pages/Scripts/tasks.js
@@ -0,0 +1,44 @@
+function addTaskWithDateTime() {
+    var taskInput = document.getElementById("taskInput");
+    var taskText = taskInput.value.trim();
+
+    var dateInput = document.getElementById("dateInput");
+    var timeInput = document.getElementById("timeInput");
+
+    var selectedDate = dateInput.value;
+    var selectedTime = timeInput.value;
+
+    if (taskText !== "") {
+        if (selectedDate !== "" && selectedTime !== "") {
+            var taskList = document.getElementById("taskList");
+            var newTask = document.createElement("li");
+            newTask.textContent = taskText;
+            taskList.appendChild(newTask);
+
+            var taskChips = document.getElementById("taskChips");
+            var taskWithDateTime = document.createElement("div");
+            taskWithDateTime.classList.add("chip");
+            var dateTimeText = "Date: " + selectedDate + ", Time: " + selectedTime;
+            taskWithDateTime.textContent = taskText + " - " + dateTimeText;
+
+            var removeButton = document.createElement("button");
+            removeButton.textContent = "Remove";
+            removeButton.onclick = function() {
+                taskChips.removeChild(taskWithDateTime);
+                taskList.removeChild(newTask);
+            };
+            taskWithDateTime.appendChild(removeButton);
+
+            taskChips.appendChild(taskWithDateTime);
+
+            // Clear all inputs after adding task with date/time
+            taskInput.value = "";
+            dateInput.value = "";
+            timeInput.value = "";
+        } else {
+            alert("Please select both date and time!");
+        }
+    } else {
+        alert("Please enter a task!");
+    }
+}
