b08d690f94191bc106989bd8e3d6f54838806056
mariammasry <mariammasry04@gmail.com>
2023-12-01 15:21:08 +0200

    generate calendar days

--- Pages/Scripts/Calendar.js
+++ Pages/Scripts/Calendar.js
@@ -29,3 +29,27 @@ const renderCalendar = () => {
       lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
       lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
     let liTag = "";
+
+ // Commit 3: Generate Calendar Days
+ for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+  }
+
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    let isToday =
+      i === date.getDate() &&
+      currMonth === new Date().getMonth() &&
+      currYear === new Date().getFullYear()
+        ? "active"
+        : "";
+    liTag += `<li class="${isToday}" data-day="${i}">
+        ${i}
+        <button class="add-task">+</button>
+        </li>`;
+  }
+
+  for (let i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
+  currentDate.innerText = `${months[currMonth]} ${currYear}`;
+  daysTag.innerHTML = liTag;
