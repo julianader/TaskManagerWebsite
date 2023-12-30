039abc4605d6c4bd00900d42ada8de988c1251b8
Mariaamhussien <m.hussien2180@nu.edu.eg>
2023-12-30 18:03:15 +0200

    Get Port Method to enable the server

--- Server.js
+++ Server.js
@@ -33,3 +33,8 @@ app.post('/register', async (req, res) => {
 app.get('/', (req, res) => {
     res.send('Server is running.');
 });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(Server is running on port ${PORT});
+});
