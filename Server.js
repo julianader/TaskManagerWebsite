19c4abf604aed10f0393554b3a7fdde1bb29760a
Julia Nader <julianader4@gmail.com>
2023-12-30 18:38:16 +0200

    Adding the post method for the login button

--- Server.js
+++ Server.js
@@ -7,7 +7,9 @@ const fs = require('fs').promises;
 
 app.use(bodyParser.json());
 app.use(cors());
+
 let registrationCounter = 1;
+
 app.post('/register', async (req, res) => {
     const { firstName, lastName, phoneNumber, email, password } = req.body;
     
@@ -30,6 +32,43 @@ app.post('/register', async (req, res) => {
         res.status(500).send('Registration failed. Please try again.');
     }
 });
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const workbook = new ExcelJS.Workbook();
+        const filePath = 'WebsiteData.xlsx';        
+        await workbook.xlsx.readFile(filePath);
+    
+        const worksheet = workbook.getWorksheet('User Data'); // Assuming your data is on the first sheet
+    
+        // Get the values from the respective columns
+        const usernameColumn = worksheet.getColumn(5).values;
+        const passwordColumn = worksheet.getColumn(6).values;
+   
+        // Replace 'searchedUsername' and 'searchedPassword' with the actual data you're searching for
+        const searchedUsername = email;
+        const searchedPassword = password;
+    
+        const rowIndex = usernameColumn.indexOf(searchedUsername);
+    
+        if (passwordColumn[rowIndex] === searchedPassword) {
+          res.send('Login successful');
+          console.log(email);
+          console.log(password);
+          console.log("Login Successful");
+        } else {
+            console.log(email);
+            console.log(password);
+            console.log("Invalid Invalid");
+          res.send('Login Failed');
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+});
+
 app.get('/', (req, res) => {
     res.send('Server is running.');
 });
